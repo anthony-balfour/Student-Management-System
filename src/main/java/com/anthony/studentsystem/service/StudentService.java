e2c8b666c89455b7ae5391ac6a0293835ae39c76
antb4 <ant5692@gmail.com>
2023-10-06 18:31:50 -0700

    backend Spring Boot files

---
+++ src/main/java/com/anthony/studentsystem/service/StudentService.java
@@ -0,0 +1,7 @@
+package com.anthony.studentsystem.service;
+
+import com.anthony.studentsystem.model.Student;
+
+public interface StudentService {
+    public Student saveStudent(Student student);
+}
---
+++ src/main/java/com/anthony/studentsystem/service/StudentServiceImpl.java
@@ -0,0 +1,16 @@
+package com.anthony.studentsystem.service;
+
+import com.anthony.studentsystem.model.Student;
+import com.anthony.studentsystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class StudentServiceImpl implements StudentService {
+
+    // Allows interaction with repository (data access layer) to perform database operations
+    @Autowired
+    private StudentRepository studentRepository;
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+}
