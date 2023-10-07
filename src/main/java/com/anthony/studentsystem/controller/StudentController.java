a3c18b6d83367c5e91b0bf0c3cdd310c1ac62d5c
antb4 <ant5692@gmail.com>
2023-10-07 14:35:14 -0700

    added PostMapping

--- src/main/java/com/anthony/studentsystem/controller/StudentController.java
+++ src/main/java/com/anthony/studentsystem/controller/StudentController.java
@@ -1,9 +1,26 @@
 package com.anthony.studentsystem.controller;
 
+import com.anthony.studentsystem.model.Student;
+import com.anthony.studentsystem.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/student")
+
 public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    // save data into the database
+
+    @PostMapping("/add")
+    public String add(@RequestBody Student student) {
+        studentService.saveStudent(student);
+        return "New student has been added";
+    }
 }
--- src/main/java/com/anthony/studentsystem/service/StudentServiceImpl.java
+++ src/main/java/com/anthony/studentsystem/service/StudentServiceImpl.java
@@ -3,7 +3,9 @@ package com.anthony.studentsystem.service;
 import com.anthony.studentsystem.model.Student;
 import com.anthony.studentsystem.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class StudentServiceImpl implements StudentService {
 
     // Allows interaction with repository (data access layer) to perform database operations
