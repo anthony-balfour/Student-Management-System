6a50a7b3aa3798c1d7f4387b7d6b0ae8bf65c065
antb4 <ant5692@gmail.com>
2023-10-07 14:42:07 -0700

    added getAllStudents which returns a List

--- src/main/java/com/anthony/studentsystem/controller/StudentController.java
+++ src/main/java/com/anthony/studentsystem/controller/StudentController.java
@@ -3,10 +3,9 @@ package com.anthony.studentsystem.controller;
 import com.anthony.studentsystem.model.Student;
 import com.anthony.studentsystem.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/student")
@@ -22,5 +21,10 @@ public class StudentController {
     public String add(@RequestBody Student student) {
         studentService.saveStudent(student);
         return "New student has been added";
+    }
+
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
     }
 }
--- src/main/java/com/anthony/studentsystem/service/StudentService.java
+++ src/main/java/com/anthony/studentsystem/service/StudentService.java
@@ -2,6 +2,11 @@ package com.anthony.studentsystem.service;
 
 import com.anthony.studentsystem.model.Student;
 
+import java.util.List;
+
 public interface StudentService {
     public Student saveStudent(Student student);
+
+    // getting list of students
+    public List<Student> getAllStudents();
 }
