8f4ae2e3599ea73a4e0c243be48b040723bfec5f
antb4 <ant5692@gmail.com>
2023-10-10 02:40:44 -0700

    finished add functionality

--- front-end/src/Components/StudentForm/studentform.js
+++ front-end/src/Components/StudentForm/studentform.js
@@ -1,14 +1,41 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { Container, Paper } from '@mui/material';
+import { Button, Container, Paper } from '@mui/material';
 
 export default function StudentForm() {
+
+  const baseURL = "http://localhost:8080/student";
 
   const paperStyle = {padding: '50px 20px', width: 600, margin: '20px auto'}
 
   const [name, setName] = useState('');
   const [address, setAddress] = useState('');
+
+  // students display in the system
+  const [students, setStudents] = useState([]);
+
+  const handleSubmitClick = (event) => {
+    event.preventDefault();
+    const student = {name, address}
+    console.log(student);
+    fetch(baseURL + "/add", {
+      method: "POST",
+      headers: {"Content-Type":"application/json"},
+      body: JSON.stringify(student)
+    }).then(() => {
+      console.log("New Student Added")
+    });
+  }
+
+  // populating the students in the database with useEffect
+  useEffect(() => {
+    fetch(baseURL + "/getAll")
+      .then(res => res.json())
+      .then((result) => {
+        setStudents(result);
+      })
+  }, []);
 
   return (
     <Box
@@ -32,6 +59,22 @@ export default function StudentForm() {
           value={address}
           onChange={(event) => setAddress(event.target.value)}
           />
+          <Button variant="contained" color="primary" onClick={handleSubmitClick}>Submit</Button>
+
+        </Paper>
+        <h1>Students</h1>
+        <Paper elevation={3} style={paperStyle}>
+          {students.map(student => {
+            return (
+            <Paper elevation={6} key={student.id} style={{margin:"10px", padding: "15px", textAlign: "left"}}>
+              <p>Id: {student.id}</p>
+              <p>Name: {student.name}</p>
+              <p>{student.address}</p>
+            </Paper>
+            )
+          })}
+
+
         </Paper>
       </Container>
     </Box>
